408059069c400ebfb4560d4e9efccfd914805dcd
Ranjith R <ranju8804@gmail.com>
2024-05-30 04:09:21 +0530

    minor changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require("cors")
 
 app.use(express.json())
 app.use(cors({
-    origin:["http://localhost:3000"],
+    origin:["http://localhost:3000","https://kseb-bill.netlify.app"],
     credentials:true
 }))
 
