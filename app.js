8e6425070240a9d0ea3f646e7ce777587f86aa98
Ranjith R <ranju8804@gmail.com>
2024-05-30 06:31:00 +0530

    minor changes

--- app.js
+++ app.js
@@ -10,8 +10,7 @@ app.use(cors({
     credentials:true
 }))
 
-app.use("/api",require("./routes/calculateRoute"))
-
+app.use("/api",require("./routes/calculateRoute"));
 
 
 app.listen(PORT,()=>console.log(`Server is running at port ${PORT}`))
